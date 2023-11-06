4acb9cd27dcb988f06dfb4c217533b853332e90e
minhtan16399 <minhtan3718a@gmail.com>
2023-11-06 21:26:15 +0700

    add code buoi 1

---
+++ src/components/homelayout/HomeLayout.jsx
@@ -0,0 +1,19 @@
+import React, { Component } from 'react'
+import Header from './Header'
+
+export default class HomeLayout extends Component {
+  render() {
+    return (
+      <div>
+        <Header />
+
+        <div className='d-flex'>
+            
+        </div>
+        
+
+        
+      </div>
+    )
+  }
+}
